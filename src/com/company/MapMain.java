272560dbd992722a1e2557945390db4e35e38794
marcygon <marciasofia49@gmail.com>
2023-02-28 15:50:05 +0100

    'mapas'

---
+++ src/com/company/MapMain.java
@@ -0,0 +1,30 @@
+package com.company;
+
+import javax.swing.*;
+import java.util.HashMap;
+import java.util.Map;
+
+public class MapMain {
+    public static void main(String[] args) {
+        Map<String, String> personas = new HashMap<>();
+
+        personas.put("138379H", "Nombre Aellido 1");
+        personas.put("138379L", "Nombre Aellido 2");
+        personas.put("138379P", "Nombre Aellido 3");
+
+        System.out.println(personas);
+
+        for(String key : personas.keySet()){//me devuelve las key id unico
+            System.out.println(key);
+        }
+        for(String value : personas.values()){//me devuelve los valores
+            System.out.println(value);
+        }
+        for(Map.Entry<String, String> pair : personas.entrySet()){//nos da un objeto que nos da ambos
+            System.out.println(pair.getKey() + " " + pair.getValue();
+        }
+    }
+}
+
+
+
