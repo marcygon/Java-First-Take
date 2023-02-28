3f063613e47b20939a5a6b2096779f941da59dbc
marcygon <marciasofia49@gmail.com>
2023-02-28 14:39:47 +0100

    'listasDinamicas'

---
+++ src/com/company/listasDinamicas/ListMain.java
@@ -0,0 +1,23 @@
+package com.company.listasDinamicas;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListMain {
+    public static void main(String[] args) {
+
+        //al hacer ctrl y click en List puedes ver toda la documentaion
+        List<String> nombres = new ArrayList<>();
+        nombres.add("Justin");
+        nombres.add("Selena");
+        nombres.add("Hailey");
+        nombres.add("Jenner");
+
+        System.out.println(nombres);//imprime la lista de array
+
+        /*el foreach imprime cada valor del array*/
+        for(String nombre:nombres){
+            System.out.println(nombre);
+        }
+    }
+}
