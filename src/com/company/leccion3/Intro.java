3f24b70f5faa8c6268d718e4e8023316d3c24628
marcygon <marciasofia49@gmail.com>
2023-03-01 13:04:53 +0100

    'funciones'

---
+++ src/com/company/leccion3/Intro.java
@@ -0,0 +1,17 @@
+package com.company.leccion3;
+
+public class Intro {
+    public static void main(String[] args) {
+        //utilizar el debug para analizar y depurar el codigo y de esa manera comprobar que el programa funciona correctamente
+        //para cambiar de nombre a un package o clase tengo que hacer clic derecho y seleccionar refractor - rename y se cambiara en todos los sitios que se utilice
+
+        /*Atajos teclado
+        *
+        * Ir a file - settings - keymap
+        * Dentro de keymap si buscas comments te saldran varios atajos y si los quieres adquirir tienes que hacer click
+        * derecho y seleccionar añadir shortcut :)
+        *
+        * */
+
+    }
+}
---
+++ src/com/company/leccion3/funciones/conParametros/Opcion1.java
@@ -0,0 +1,15 @@
+package com.company.leccion3.funciones.conParametros;
+
+public class Opcion1 {
+    public static void main(String[] args) {
+        saludar("Hola terricola");
+        presentacion("Márcia", 23);
+    }
+
+    static void saludar(String saludo){
+        System.out.println(saludo + " espero que estes teniendo un buen dia.");
+    }
+    static void presentacion(String nombre, int edad){
+        System.out.println("Me llamo " + nombre + " y tengo " + edad + " años.");
+    }
+}
