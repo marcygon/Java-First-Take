d10b890ff5ddd100234f8a15aa6a5f554528d8f0
marcygon <marciasofia49@gmail.com>
2023-03-02 11:15:10 +0100

    'clasesAbstractas5.10'

--- src/com/company/lecciones/leccion5/clases/Main.java
+++ src/com/company/lecciones/leccion5/clases/Main.java
@@ -33,7 +33,15 @@ public class Main {
         colonia = new HandCream();
         colonia.perfumar(3);
 
-        
+        /**
+         * clase abstracta: no se puede instanciar(crear objeto), solo instancian las clases hijas
+         *
+         * la clase abstracta se coloca en la super clase, por ejemplo en mi caso es Colonia. Lo que pasa
+         * es que no me dejara crear objetos de colonia, sino que me obliga a crear objetos de la clase
+         * BodyMilk, HandCream o Perfume. Asi se que es cada cosa y no es algo en general como Colonia <3
+         */
+
+
 
     }
 }
