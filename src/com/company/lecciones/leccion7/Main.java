d7722274e316c64aa741bab5043631a6c061a7a2
marcygon <marciasofia49@gmail.com>
2023-03-08 21:15:48 +0100

    'refactoring7.1'

---
+++ src/com/company/lecciones/leccion7/Main.java
@@ -0,0 +1,16 @@
+package com.company.lecciones.leccion7;
+
+public class Main {
+    public static void main(String[] args) {
+        //el refactoring se utiliza para reducir la complegidad del codigo y hacerlo más legible
+        /**
+         * facilita el mantenimiento a largo plazo
+         * facilita la edicion
+         * sencillo
+         *
+         * si se hace click derecho en cualquier parte del codigo => generate => refactor, accedemos a las
+         * distintas tecnicas de refactorizacion. Dependiendo de donde nos situemos nos ofrece unas tecnicas
+         * u otras
+         */
+    }
+}
